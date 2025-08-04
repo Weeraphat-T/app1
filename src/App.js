e73e12be5ddc5c54a8beda10bdf2132c43d48022
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-08-04 11:06:28 +0700

    4-8-2025 Part 3
    Event Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from 'react'
-import {Calculator2} from './calculator';
+import {EventData2} from './event-data';
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;

fc233b84aa6e3a40b111cc232e10c5ee826c5215
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-08-04 09:55:11 +0700

    4-8-2025 Part 1
    Change to Botton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import {Header,Content,Footer} from './func-components'
-import Calendar from './class-component';
+import Botton from './class-component';
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  );
+  return <Botton/>
 }
 
 export default App;

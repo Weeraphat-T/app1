769fbae788be2d8f49c8d744f3e5b27d52bc2120
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-08-13 14:22:18 +0700

    13-8-2025
    Refs With Table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import React from 'react'
-import {EventData2} from './event-data';
+import RefsArray from './refs-array';
 
-function App() {
-  return <EventData2/>
+export default function App() {
+  return <RefsArray/>
 }
-
-export default App;

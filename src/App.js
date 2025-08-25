408a9531a588aca937d3c4f7b14b577f59df11fb
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-08-25 10:20:41 +0700

    Effect & Context
    25/8/2025

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
 import React from 'react'
-import MassageBox from './state-func';
+import {userContext} from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 export default function App() {
-  return <MassageBox/>
+  let [user, setUser] = React.useState('')
+
+  return (
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+  )
 }

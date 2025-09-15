0403f3a833f73758fd15b697d4daae4b0cba526f
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-09-15 11:29:58 +0700

    bootstrap File

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@
 // export default App;
 
 import React from 'react'
-import FormText from './form-text'
+import FormSelectFile from './form-select-file'
 
 export default function App() {
-  return <FormText/>
+  return <FormSelectFile/>
 }
